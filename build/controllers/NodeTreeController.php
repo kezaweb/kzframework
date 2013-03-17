4305a5e85b867558e8c215e4db989b5ec0fc135c
Yoann - Kezaweb <kezaweb@gmail.com>
2013-03-17 17:29:36 +0100

    First commit for a small projet. Allow using JsTree with PropelOrm

--- build/controllers/NodeTreeController.php
+++ build/controllers/NodeTreeController.php
@@ -1,6 +1,8 @@
 <?php
 namespace Kzf\Controller;
 
+use Kzf\Model\NodeTreeQuery;
+
 use Symfony\Component\HttpFoundation as Http;
 use Kzf\Controller\Base as Base;
 use Kzf\Model as Model;
@@ -11,8 +13,18 @@ class NodeTreeController extends Base\BaseController
 	public function singleAction() {
 		$aData = json_decode($this->request->get('jData'),true);
 		$aData = !is_array($aData)?array():$aData;
-		$aResponse = Engine\KzfObject::CUDObject($aData, 'Kzf\Model\NodeTree');
-		return $this->renderJson(json_encode($aResponse));
+		if (array_key_exists('action', $aData) && $aData['action']=='read') {
+			/* @var $oNodeTree Model\NodeTree */
+			$oNodeTree = Engine\KzfObject::findByData($aData);
+			if ($oNodeTree->getFormat()=='text/json') {
+				return $this->renderJson(json_encode($oNodeTree->getDataResponse()));
+			} else {
+				$this->renderBackend($oNodeTree->getTemplate(),$oNodeTree);
+			}
+		} else {
+			$aResponse = Engine\KzfObject::CUDObject($aData, 'Kzf\Model\NodeTree');
+			return $this->renderJson(json_encode($aResponse));			
+		}
 	}
 	
 	public function listAction() {
